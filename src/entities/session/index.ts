906ef0d00d4f4003197b96896e0995513aacedcf
dimon <dimonmezey@gmail.com>
2023-12-18 05:48:18 +0200

    fix

--- src/entities/session/index.ts
+++ src/entities/session/index.ts
@@ -4,3 +4,4 @@ export { useRoleUser } from './model/sessionModel';
 export { AuthGuard } from './ui/auth-guard/AuthGuard';
 export { SuperAdminGuard } from './ui/super-admin-guard/SuperAdminGuard';
 export { PatientsGuard } from './ui/patients-guard/PatientsGuard';
+export { PersonnelGuard } from './ui/personnel-guard/PersonnelGuard';
