8602b04be35e69bcc9ee1c2ee493540a8eca8684
DimonONE <dimonmezey@gmail.com>
2024-04-04 03:13:33 +0300

    deploy

--- dist/assets/index-b218a304.js
+++
@@ -1,1 +0,0 @@
-import{aP as o,aQ as t,u as r,r as a,P as u}from"./index-fa80df68.js";import{s as n}from"./sessionApi-ff99e280.js";import"./useQuery-229aca3d.js";import"./useBaseQuery-e8d907b8.js";function i(e){o(),e.removeQueries(n.session.currentUser())}function m(){const e=t(),s=r();return a.useEffect(()=>{i(e),s(u.login)},[]),null}export{m as default};
--- dist/assets/styles.module-43fe270e.js
+++
@@ -1,1 +0,0 @@
-const o="_root_srd1e_1",s="_checked_srd1e_5",c={root:o,checked:s},t="_root_em3fq_1",e={root:t};export{e as a,c as s};
