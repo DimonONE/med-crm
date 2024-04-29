1d77cded75e092b8ce06338a2a171339feaccbe0
DimonONE <dimonmezey@gmail.com>
2024-04-29 14:36:50 +0300

    fix: navigate button

--- src/features/navigate/lib/navigate-button/utils.ts
+++ src/features/navigate/lib/navigate-button/utils.ts
@@ -6,22 +6,26 @@ export const getNavigateList = (
   switch (true) {
     case checkUserRole('superAdmin'):
       return [
-        { value: PATH_PAGE.superAdmin.clinics, label: 'Все клиники' },
-        { value: PATH_PAGE.superAdmin.addClinic, label: 'Добавить' },
+        { value: PATH_PAGE.superAdmin.clinics, label: 'Клиники' },
+        { value: PATH_PAGE.superAdmin.addClinic, label: 'Добавить клинику' },
         { value: PATH_PAGE.superAdmin.clinicApplications, label: 'Заявки' },
       ];
 
     case checkUserRole('medChief'):
       return [
         { value: PATH_PAGE.personnel.root, label: 'Персонал' },
+        { value: PATH_PAGE.patients.records, label: 'Пациенты' },
+        { value: PATH_PAGE.doctor.root, label: 'Журнал' },
         { value: PATH_PAGE.attendance.root, label: 'Посещаемость' },
-        { value: PATH_PAGE.personnel.add, label: 'Добавить' },
+        { value: PATH_PAGE.services, label: 'Услуги' },
       ];
 
     case checkUserRole('doctor'):
       return [
         { value: PATH_PAGE.patients.records, label: 'Пациенты' },
-        { value: PATH_PAGE.patients.add, label: 'Добавить' },
+        { value: PATH_PAGE.patients.add, label: 'Добавить пациента' },
+        { value: PATH_PAGE.doctor.root, label: 'Журнал' },
+        { value: PATH_PAGE.attendance.root, label: 'Посещаемость' },
       ];
 
     default:
